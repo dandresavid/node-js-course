51e6b526c11211b9f86194d44770aa7952806c09
Andres David Blandon <dandresavid@hotmail.com>
2022-09-23 18:35:24 +0200

    Added url for the forecast API

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,9 +3,10 @@ const forecast = require('./utils/forecast')
 const fs = require('fs')
 
 
+
 const address = process.argv[2]
 if (!address) {
-  console.log('PLease provide an address')
+  console.log('Please provide an address')
 } else {
   geocode(address, (error, {latitude,longitude,location} = {}) => {
     if  (error){
--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -1,14 +1,16 @@
 const request = require('request')
 
-const geocode = (latitud,longitud, callback) => {
-    const url = 'https://api.darksky.net/forecast/82e142295847a304eebf3fcc1eb95a8e/' + latitud + ',' + longitud + '?units=si'
-    request({ url, json: true }, (error, {body}) => {
+
+const forecast = (latitud,longitud, callback) => {
+    const url2 = 'http://api.weatherstack.com/current?access_key=9ddf50d9b53132f5f46620aaa9842b4d&query=' + latitud + ',' + longitud + '&units=f';
+    const url = 'https://api.darksky.net/forecast/82e142295847a304eebf3fcc1eb95a8e/' + latitud + ',' + longitud 
+    request({ url: url, json: true }, (error, {body}) => {
         if (error) {
-            callback('Unable to connect to weather services!', undefined)
+            callback(`Unable to connect to weather services! ${body}`, undefined)
         } else if (body.error) {
-            callback('Unable to find location. Try another search.', undefined)
+            callback(`Unable to find location. Try another search.${body}`, undefined)
         } else {
-            callback(undefined, {
+            callback(undefined, { 
                 precipProbability: body.currently.precipProbability,
                 temperature: body.currently.temperature,
                 summary: body.currently.summary
@@ -17,4 +19,4 @@ const geocode = (latitud,longitud, callback) => {
     })
 }
 
-module.exports = geocode
+module.exports = forecast
