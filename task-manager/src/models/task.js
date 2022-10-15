4f72cabe7f27bd416e54bcbdd9626471d886463b
Andres David Blandon <dandresavid@hotmail.com>
2022-10-15 09:09:35 +0300

    Added the create and read ednpoints for users and tasks

---
+++ task-manager/src/models/task.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+//Model definition for Tasks
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+})
+
+module.exports = Task
